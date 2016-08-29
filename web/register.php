84fbf0e517fa925f666b3ddba7fd4d9cf0cb2470
tomex <android.tomex@gmail.com>
2016-08-29 11:50:08 +0900

    update commit.

--- web/register.php
+++ web/register.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 	$input = file_get_contents("php://input");
 	$json = @json_decode($input,true);
 	if(!isset($json["name"]))
-		die(json_encode($jsonput));
+		die(json_encode($output));
 	$userdata = null;
 	if(isset($json["token"]) && isset($json["tokenSecret"]) && isset($json["service"]))
 		$userdata = new ServiceLoginUserData($json["token"],$json["tokenSecret"],$json["service"]);
